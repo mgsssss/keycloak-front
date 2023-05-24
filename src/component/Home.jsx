9d23fa2514895bef87d267e4a9242a434048bff7
mgsssss <mgsssss@naver.com>
2023-05-24 14:44:39 +0900

    css

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,17 +1,17 @@
 import UserService from "../services/UserService";
-import jwtDecode from "jwt-decode";
 
 const Home = (props) => {
     console.log(props);
     const token = UserService.getIdToken()
     console.log("~!~~~~");
     console.log(UserService.getuserInfo());
+    console.log(UserService.getLoadUserInfo());
     console.log("~!~~~~");
     return (
-        <div className="bg-light p-5 rounded-lg m-3">
+        <div className="bg-light p-5 rounded-lg m-3" >
             <p className="lead">Signed in as {UserService.getUsername()}</p>
+            {UserService.getuserInfo}
             <div>
-                
                 
             </div>
         </div>
